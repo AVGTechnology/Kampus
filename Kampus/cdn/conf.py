3037d75e3a930719811ae9a6a1224435486994b7
AVGTechnology <africavaluedgaintstechnology@gmail.com>
2021-12-09 10:57:42 +0100

    Added Space storage5

--- Kampus/cdn/conf.py
+++ Kampus/cdn/conf.py
@@ -3,7 +3,7 @@ import os
 AWS_ACCESS_KEY_ID = 'FMAGECPBUNCWXWAIIVOD'
 AWS_SECRET_ACCESS_KEY = 'N1MX6oStDkTXglMLGBMXRf7CjzayFEJ+ihaVsGJX/OA'
 AWS_STORAGE_BUCKET_NAME = 'kampusstorage'
-AWS_S3_ENDPOINT_URL = 'fra1.digitaloceanspaces.com'
+AWS_S3_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
