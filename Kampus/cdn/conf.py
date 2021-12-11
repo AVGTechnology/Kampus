dc9241d9b60b37ab343d5535ea9d79377f91be04
AVGTechnology <africavaluedgaintstechnology@gmail.com>
2021-12-12 00:16:25 +0100

    predploy

--- Kampus/cdn/conf.py
+++ Kampus/cdn/conf.py
@@ -4,6 +4,7 @@ AWS_ACCESS_KEY_ID = 'FMAGECPBUNCWXWAIIVOD'
 AWS_SECRET_ACCESS_KEY = 'N1MX6oStDkTXglMLGBMXRf7CjzayFEJ+ihaVsGJX/OA'
 AWS_STORAGE_BUCKET_NAME = 'kampusstorage'
 AWS_S3_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
+AWS_QUERYSTRING_AUTH = False  # it will remove the authentication info from the url
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
@@ -11,4 +12,4 @@ AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.fra1.digitaloceanspaces.com"
 
 DEFAULT_FILE_STORAGE = "Kampus.cdn.backend.MediaRootS3Boto3Storage"
 STATICFILES_STORAGE = "Kampus.cdn.backend.StaticRootS3Boto3Storage"
-#MEDIA_URL = "https://fra1.digitaloceanspaces.com/kampusstorage/"
+# MEDIA_URL = "https://fra1.digitaloceanspaces.com/kampusstorage/"
